d13202a17bb8f70ce2db75b21bd307a6b4b8629b
Aurino <aurinodoc@gmail.com>
2023-10-17 16:48:06 -0300

    Feat: arquivo de consultas ao BD.

--- pessoa_db.php
+++ pessoa_db.php
@@ -1,5 +1,12 @@
 <?php
-    /*
-    lista_pessoa(){
+    function lista_pessoa($id){
 
-    };
+        $conexao = mysqli_connect("localhost","root","","HORUS_TREINO");
+
+        mysqli_select_db($conexao,'HORUS_TREINO');
+
+        $query_list="SELECT * FROM Pessoa ORDER BY Id";
+
+        $list_pessoas=mysqli_query($conexao,$query_list);
+
+    }
