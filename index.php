a5104a2dff362ee0d9af8b11acc02f0b27525365
Aurino <aurinodoc@gmail.com>
2023-10-18 03:37:52 -0300

    Feat: Pasta com arquivo de banco de dados para melhor separação.

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
 <?php
-
-        $conexao= mysqli_connect('localhost','root','','HORUS_TREINO');
-
-        mysqli_select_db($conexao,'HORUS_TREINO');
+        require_once('./db/pessoa_db.php');
 
         if(!empty($_GET['action']) AND $_GET['action']=="delete"){
 
             $id = (int) $_GET['id'];
-            $result=mysqli_query($conexao,"DELETE FROM Pessoa WHERE Id ='{$id}'");
 
+            exclui_pessoa($id);
         }
 
-        $query_list="SELECT * FROM Pessoa ORDER BY Id";
-
-        $list_pessoas=mysqli_query($conexao,$query_list);
+        $pessoas=lista_pessoas();
 
         $items='';
 
-        while ($linha = mysqli_fetch_assoc($list_pessoas)) {
-            $item = file_get_contents('./html/item.html');
-            $item = str_replace('{id}', $linha['Id'], $item);
-            $item = str_replace('{nome}', $linha['Nome'], $item);
-            $item = str_replace('{telefone}', $linha['Telefone'], $item);
-            $item = str_replace('{email}', $linha['Email'], $item);
-            $item = str_replace('{endereco}', $linha['Endereco'], $item);
-            $item = str_replace('{bairro}', $linha['Bairro'], $item);
-            $items.= $item;
+        if($pessoas){
+            foreach ($pessoas as $pessoa){
+                $item = file_get_contents('./html/item.html');
+                $item = str_replace('{id}', $pessoa['Id'], $item);
+                $item = str_replace('{nome}', $pessoa['Nome'], $item);
+                $item = str_replace('{telefone}', $pessoa['Telefone'], $item);
+                $item = str_replace('{email}', $pessoa['Email'], $item);
+                $item = str_replace('{endereco}', $pessoa['Endereco'], $item);
+                $item = str_replace('{bairro}', $pessoa['Bairro'], $item);
+                $items.= $item;
+            }
         }
 
         $list = file_get_contents('./html/list.html');
--- pessoa_form_insert.php
+++ pessoa_form_insert.php
@@ -1,27 +1,21 @@
     <?php
-        $id =$nome=$endereco=$email=$bairro=$telefone ='';
-        if(!empty($_REQUEST['action'])){
-            $conexao= mysqli_connect('localhost','root','','HORUS_TREINO');
 
-            mysqli_select_db($conexao,'HORUS_TREINO');
+        require_once('./db/pessoa_db.php');
+
+        if(!empty($_REQUEST['action'])){
 
             if($_REQUEST['action'] == 'edit'){
                 $id = (int) $_GET['id'];
-                
-                $query_select_pessoa="SELECT * FROM Pessoa WHERE Id = '{$id}' ";
-
-                $result_pessoa=mysqli_query($conexao,$query_select_pessoa);
-            
-                if($linha=mysqli_fetch_array($result_pessoa)){
-                    $id=$linha['Id'];
-                    $nome=$linha['Nome'];
-                    $telefone=$linha['Telefone'];
-                    $email=$linha['Email'];
-                    $endereco=$linha['Endereco'];
-                    $bairro=$linha['Bairro'];
-                }
+                $pessoa = get_pessoa($id);//Direto do banco
+                $id=$pessoa['Id'];
+                $nome=$pessoa['Nome'];
+                $endereco=$pessoa['Endereco'];
+                $email=$pessoa['Email'];
+                $telefone=$pessoa['Telefone'];
+                $bairro=$pessoa['Bairro'];
             } 
             else if ($_REQUEST['action'] =='save'){
+                $pessoa=$_POST; //Vindo do POST do formulário. (Notar letras minusculas em relação as chaves vindas do banco)
                 $id=$_POST['id'];
                 $nome=$_POST['nome'];
                 $telefone=$_POST['telefone'];
@@ -29,45 +23,23 @@
                 $endereco=$_POST['endereco'];
                 $bairro=$_POST['bairro'];
                 if(empty($_POST['id'])){
-                    $query_increment="SELECT MAX(Id) AS maior_id FROM Pessoa";
-
-                    $retorno_max_id= mysqli_query($conexao,$query_increment);
-
-                    $linha = mysqli_fetch_assoc($retorno_max_id);
-                    $maior_id = (int) $linha['maior_id'];
-
-                    $novo_id=$maior_id+1;
-
-                    $query_insert="INSERT INTO Pessoa (Id,Nome, Telefone, Email, Endereco, Bairro) VALUES ('$novo_id','$nome','$telefone','$email','$endereco','$bairro')"; 
-                    
-                    mysqli_query($conexao,$query_insert);
+                    $pessoa['id']=get_next_pessoa();
+                    $result=insert_pessoa($pessoa);
                 }else{
-
-                    $query_save_update="UPDATE Pessoa SET Nome = '{$nome}',
-                    Telefone = '{$telefone}',
-                    Email = '{$email}',
-                    Bairro = '{$bairro}',
-                    Endereco = '{$endereco}'
-                    WHERE Id = '{$id}';
-                    ";
-
-                    $result_pessoa_save=mysqli_query($conexao,$query_save_update);
-
+                    $result=update_pessoa($pessoa);
                 }
                 echo "<script>
                         alert('Registro inserido com sucesso!');
                     </script>";
-                mysqli_close($conexao);
             }
         }
         else{
-            $_POST = [];
-            $_POST['id'] = '';
-            $_POST['nome'] = '';
-            $_POST['endereco'] = '';
-            $_POST['bairro'] = '';
-            $_POST['telefone'] = '';
-            $_POST['email'] = '';
+            $id = '';
+            $nome = '';
+            $endereco = '';
+            $bairro = '';
+            $telefone = '';
+            $email = '';
         }
 
         $form = file_get_contents('html/form.html');
